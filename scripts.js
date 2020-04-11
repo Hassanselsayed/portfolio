8cb960e8b9a0809c8fd9b6e0cbb367f6f82c5b90
Hassanselsayed <hsaadelsayed@gmail.com>
2020-04-10 22:09:49 -0400

    UI Improvements

--- scripts.js
+++ scripts.js
@@ -2,16 +2,20 @@ $(function () {
     $('.hamburger').on('click', function () {
         // toggle class show on the menu
         $('.menu').addClass('show');
+        $('.social-links').addClass('hide-links');
     });
+
 
     $('.exit-responsive').on('click', function () {
         // toggle class show on the menu
         $('.menu').removeClass('show');
+        $('.social-links').removeClass('hide-links');
     });
 
     $('.menu a').on('click', function () {
         // toggle class show on the menu
         $('.menu').removeClass('show');
+        $('.social-links').removeClass('hide-links');
     });
 
     $('a').on('click', function () {
